8d860c51273f7e47f337df4af3b8694ca6b90ae1
Jim Huang <jim.javathunderbird@gmail.com>
2025-12-06 21:33:34 -0800

    use details

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-
 <?php
 function get_random_questions() {
     $json = file_get_contents('questions.json');
     $all_questions = json_decode($json, true);
-    $keys = array_rand($all_questions, min(4, count($all_questions)));
-    if (!is_array($keys)) {
-        $keys = [$keys];
-    }
-    $questions = [];
-    foreach ($keys as $key) {
-        $questions[] = $all_questions[$key];
-    }
+    shuffle($all_questions);
+    $questions = array_slice($all_questions, 0, 4);
     return $questions;
 }
 
@@ -19,7 +12,7 @@ $questions = get_random_questions();
 
 <script>
 function show_alert_message(row_number, column_number, question) {
-    alert("you clicked on row " + row_number + ", column " + column_number + ", question id: " + question.id);
+    alert(`you clicked on row ${row_number}, column ${column_number}, question id: ${question.id}`);
 }
 </script>
 
@@ -32,30 +25,34 @@ function show_alert_message(row_number, column_number, question) {
             </tr>
         </thead>
         <tbody>
-            <?php $row_index = 1; foreach ($questions as $question): ?>
-            <tr>
-                <td class="table-cell" onclick='show_alert_message(<?php echo $row_index; ?>, 1, <?php echo json_encode($question); ?>)'><?php echo htmlspecialchars($question['id']); ?></td>
-                <td class="table-cell" onclick='show_alert_message(<?php echo $row_index; ?>, 2, <?php echo json_encode($question); ?>)'><?php echo htmlspecialchars($question['body']); ?></td>
-            </tr>
-            <?php $row_index++; endforeach; ?>
+            <?php $row_index = 1; ?>
+            <?php foreach ($questions as $question): ?>
+                <tr>
+                    <td class="body-cell" onclick='show_alert_message(<?= $row_index ?>, 1, <?= json_encode($question) ?>)'><?= htmlspecialchars($question['id']) ?></td>
+                    <td class="body-cell" onclick='show_alert_message(<?= $row_index ?>, 2, <?= json_encode($question) ?>)'><?= htmlspecialchars($question['body']) ?></td>
+                </tr>
+                <?php $row_index++; ?>
+            <?php endforeach; ?>
         </tbody>
     </table>
 </div>
 
 <div id="questions-list">
-    <?php $para_index = 0; foreach ($questions as $question): ?>
-    <p class="paragraph <?php echo ($para_index % 2 == 0) ? 'paragraph-even' : 'paragraph-odd'; ?>">
-        <?php echo htmlspecialchars($question['body']); ?>
-    </p>
-    <?php $para_index++; endforeach; ?>
+    <?php $para_index = 0; ?>
+    <?php foreach ($questions as $question): ?>
+        <p class="paragraph-item <?= $para_index % 2 === 0 ? 'para-even' : 'para-odd' ?>">
+            <?= htmlspecialchars($question['body']) ?>
+        </p>
+        <?php $para_index++; ?>
+    <?php endforeach; ?>
 </div>
 
 <style>
 #questions-table table {
-    width: 100%;
+    background: lightYellow;
     border: 1px solid blue;
     border-collapse: collapse;
-    background: lightYellow;
+    width: 100%;
 }
 
 #questions-table .header-cell {
@@ -65,7 +62,7 @@ function show_alert_message(row_number, column_number, question) {
     border: 1px solid blue;
 }
 
-#questions-table .table-cell {
+#questions-table .body-cell {
     background: wheat;
     text-align: right;
     padding: 10px;
@@ -73,34 +70,34 @@ function show_alert_message(row_number, column_number, question) {
     cursor: pointer;
 }
 
-#questions-list .paragraph {
+#questions-list .paragraph-item {
     margin-left: 10px;
     margin-right: 10px;
 }
 
-#questions-list .paragraph-odd {
+#questions-list .para-odd {
     background: yellow;
 }
 
-#questions-list .paragraph-even {
-    background: lightYellow;
+#questions-list .para-even {
+    background: lightyellow;
 }
 
-@media screen and (max-width: 768px) {
+@media (max-width: 768px) {
     #questions-table table {
         font-size: 14px;
     }
     
-    #questions-table .table-cell,
-    #questions-table .header-cell {
+    #questions-table .header-cell,
+    #questions-table .body-cell {
         padding: 8px;
     }
     
-    #questions-list .paragraph {
+    #questions-list .paragraph-item {
+        margin-left: 5px;
+        margin-right: 5px;
         font-size: 14px;
-        padding: 8px;
     }
 }
 </style>
-
 
