f9c2e355c954fd4e48db9471f534f2a4e2424a23
Jim Huang <jim.javathunderbird@gmail.com>
2025-12-06 19:55:05 -0800

    update style

--- index.php
+++ index.php
@@ -1,18 +1,50 @@
+✓ List directory . (15 files)
+
+✓ Read questions.json (6 lines)
+
 ```php
 <?php
 function get_random_questions() {
     $json = file_get_contents('questions.json');
     $all_questions = json_decode($json, true);
     shuffle($all_questions);
-    return array_slice($all_questions, 0, 4);
+    $questions = array_slice($all_questions, 0, 4);
+    return $questions;
 }
 
 $questions = get_random_questions();
 ?>
 
+<!-- questions-table -->
+<table id="questions-table">
+    <thead>
+        <tr>
+            <th class="table-header">ID</th>
+            <th class="table-header">QUESTION</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($questions as $index => $question): ?>
+        <tr>
+            <td class="table-cell" onclick="show_alert_message(<?php echo $index + 1; ?>, 1)"><?php echo htmlspecialchars($question['id']); ?></td>
+            <td class="table-cell" onclick="show_alert_message(<?php echo $index + 1; ?>, 2)"><?php echo htmlspecialchars($question['body']); ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<!-- questions-list -->
+<div id="questions-list">
+    <?php foreach ($questions as $index => $question): ?>
+    <p class="paragraph <?php echo ($index % 2 === 0) ? 'paragraph-even' : 'paragraph-odd'; ?>">
+        <?php echo htmlspecialchars($question['body']); ?>
+    </p>
+    <?php endforeach; ?>
+</div>
+
 <script>
 function show_alert_message(row_number, column_number) {
-    alert("you are clicking on row " + row_number + ", column " + column_number);
+    alert("you clicked on row " + row_number + ", column " + column_number);
 }
 </script>
 
@@ -20,74 +52,50 @@ function show_alert_message(row_number, column_number) {
 #questions-table {
     background: lightYellow;
     border: 1px solid blue;
+    width: 100%;
     border-collapse: collapse;
+}
+
+.table-header {
+    background: lightYellow;
+    text-align: left;
+    padding: 10px;
+}
+
+.table-cell {
+    background: wheat;
+    text-align: right;
+    padding: 10px;
+    cursor: pointer;
+}
+
+#questions-list {
     width: 100%;
 }
 
-#questions-table th {
-    background: lightYellow;
-    text-align: left;
+.paragraph {
+    margin-left: 10px;
+    margin-right: 10px;
 }
 
-#questions-table td {
-    background: wheat;
-    text-align: right;
-    cursor: pointer;
-    padding: 8px;
-}
-
-#questions-table th,
-#questions-table td {
-    border: 1px solid blue;
-}
-
-#questions-list .paragraph-odd {
+.paragraph-odd {
     background: yellow;
-    padding: 10px;
-    margin: 5px 0;
 }
 
-#questions-list .paragraph-even {
-    background: lightyellow;
-    padding: 10px;
-    margin: 5px 0;
+.paragraph-even {
+    background: lightYellow;
 }
 
-@media (max-width: 768px) {
-    #questions-table {
+@media screen and (max-width: 768px) {
+    .table-cell, .table-header {
         font-size: 14px;
+        padding: 8px;
     }
     
-    #questions-table td,
-    #questions-table th {
-        padding: 6px;
+    .paragraph {
+        font-size: 14px;
     }
 }
 </style>
-
-<table id="questions-table">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>QUESTION</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php $row_index = 1; foreach ($questions as $question): ?>
-        <tr>
-            <td onclick="show_alert_message(<?php echo $row_index; ?>, 1)"><?php echo htmlspecialchars($question['id']); ?></td>
-            <td onclick="show_alert_message(<?php echo $row_index; ?>, 2)"><?php echo htmlspecialchars($question['body']); ?></td>
-        </tr>
-        <?php $row_index++; endforeach; ?>
-    </tbody>
-</table>
-
-<div id="questions-list">
-    <?php $para_index = 1; foreach ($questions as $question): ?>
-    <p class="paragraph-<?php echo ($para_index % 2 == 1) ? 'odd' : 'even'; ?>">
-        <?php echo htmlspecialchars($question['id'] . ': ' . $question['body']); ?>
-    </p>
-    <?php $para_index++; endforeach; ?>
-</div>
 ```
 
--- php_web_agent.php
+++ php_web_agent.php
@@ -1,22 +1,12 @@
-You are a PHP version 8.4.13 coding expert, please generate the php codeblock base on the <toml_spec>
-
-- for [components.*], only include their related html code, do not include html document tags like <!DOCTYPE html>, <html>, <head>, <body>...
-- for [logic.php.<name>], make the php function name same as <name>
-  for example:
-  [logic.php.hello_world] corresponds to a php function hello_world
-- for [logic.js.<name>], make the javascription function name same as <name>
-  for example:
-  [logic.js.hello_world] corresponds to a javascript function hello_world
-- do not use echo or heredoc to output html
-- do not use inline css
-- do not use inline javascript
-- for all the css, make sure it works on both desktop web browser and mobile web browser
-- do not include any mock testing code
-- show me the code only, no explanation and no extra words
+<?php require_once "php_web_system_instruction.txt"; ?>
 
 output in the following format:
 <?php
+echo <<<CODE
+<?php
 // all php logic code here...
+?>
+CODE;
 ?>
 
 <script>
